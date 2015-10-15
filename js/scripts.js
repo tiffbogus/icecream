9b91fba2c2f8cd9ecf13d10fae8d07c6e836d518
lauryn davis & Tiffany Bogus <student@epicodus.com>
2015-10-14 20:56:15 -0700

    Add stuff.

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+     blanks.forEach(function(blank) {
+         var userInput = $("input#" + blank).val();
+         $("." + blank).text(userInput);
+
+         $("#story").show();
+
+         event.preventDefault();
+           });
+     });
+
+
+        var elements = ["h1", "p"];
+
+        elements.forEach(function(element) {
+        $(elements).click(function() {
+        alert('This is a ' + element + '.');
+
+    });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     var person1Input = $("input#person1").val()
+//     var person2Input = $("input#person2").val()
+//     var animalInput= $("input#animal").val()
+//     var exclamationInput = $("input#exclamation").val()
+//     var verbInput = $("input#verb").val()
+//     var nounInput = $("input#noun").val()
+//
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+// });
